778d72991fa10ffba2e5ddbc53c28f3970724c3c
Lexi Cummins <lcummins@burns-group.com>
2021-08-09 11:05:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Components/Create";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
