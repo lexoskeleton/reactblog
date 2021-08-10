12c50f7d7093e343051c9e8d2a77a4f4250cda4d
lex <lcummins@burns-group.com>
2021-08-10 14:16:45 -0400

    handlesubmit function

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -3,11 +3,19 @@ import { useState } from "react";
 const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog)
+
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title</label>
                 <input 
                 type="text"
@@ -23,13 +31,17 @@ const Create = () => {
                 >
                 </textarea>
                 <label>Blog Author</label>
-                <select>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
                 <option value="mario">mario</option>
                 <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
                 <p>{title}</p>
                 <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
       );
