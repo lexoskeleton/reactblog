9c73cd2691f03a1745acc642a8bf1650c17e388e
lex <lcummins@burns-group.com>
2021-08-10 13:59:43 -0400

    added css

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title</label>
+                <input 
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value) }
+                />
+                <label>Blog Body</label>
+                <textarea
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                >
+                </textarea>
+                <label>Blog Author</label>
+                <select>
+                <option value="mario">mario</option>
+                <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+            </form>
         </div>
       );
 }
